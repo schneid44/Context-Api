147a4981d23dad597a6d14eab1258742f8fdd10c
schneid44 <justinschneider44@gmail.com>
2021-03-28 03:51:51 -0400

    All files

---
+++ src/Button.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { ThemeContextConsumer } from "./ThemeContext";
+
+function Button(props) {
+  return (
+    <ThemeContextConsumer>
+      {(context) => (
+        <button onClick={context.toggleTheme} className="button">
+          Switch
+          <span role="img" aria-label="sun">
+            ?
+          </span>
+          <span role="img" aria-label="moon">
+            ?
+          </span>
+        </button>
+      )}
+    </ThemeContextConsumer>
+  );
+}
+export default Button;
